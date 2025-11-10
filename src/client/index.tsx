7ed462bb4f7b659585e89e8b8ad98792b82ed3f9
craqpter <54701577+craqpter@users.noreply.github.com>
2025-11-10 08:37:34 +0200

    Update index.tsx

--- src/client/index.tsx
+++ src/client/index.tsx
@@ -447,7 +447,10 @@ function App() {
             />
           </a>
          <a href="https://l2anons.adenium.online/index.php" target="_blank">
-           <img src="https://l2anons.adenium.online/assets/l2anonsknopka.jpg" alt="L2Anons Adenium" style="width:160px; height:auto; border:none;">
+           <img 
+             src="https://l2anons.adenium.online/assets/l2anonsknopka.jpg" 
+             alt="L2Anons Adenium" 
+             style="width:160px; height:auto; border:none;">
          </a>
         </div>
       </section>
